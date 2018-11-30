260e79e2ed6a666cba0d7284055e84ffc4f5c0b1
Jimmy Rios Leung <jimmy.leung@7comm.com.br>
2018-11-30 17:11:19 -0200

    challenge 7: basic recursion w/ reduce

---
+++ basic-recursion/program.js
@@ -0,0 +1,7 @@
+function reduce(arr, fn, initial) {
+    if (!arr.length) return initial;
+    initial[arr[0]] = initial[arr[0]] ? initial[arr[0]] + 1 : 1;
+    return reduce(arr.slice(1), fn, initial);
+}
+
+module.exports = reduce;
